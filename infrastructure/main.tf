a3870e684c4b97862d4d04093c4c5de7b944efdd
TheCoolDrop <vanio.begic123@gmail.com>
2022-12-29 08:33:22 +0100

    Add Terraform and ansible infra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,6 +13,10 @@ terraform {
       source = "hashicorp/random"
       version = "~> 3.0"
     }
+    local = {
+      source = "hashicorp/local"
+      version = "2.2.3"
+    }
   }
 }
 
@@ -69,4 +73,40 @@ resource "aws_lightsail_database" "traccar_database" {
   publicly_accessible = false
   skip_final_snapshot = false
   final_snapshot_name = random_pet.final_database_snapshot_name.id
+}
+
+
+resource "local_sensitive_file" "lightsail_private_key" {
+  content = aws_lightsail_key_pair.ligtsail_key.private_key
+  filename = "/home/vanio/LightsailPrivateKey.pem"
+  file_permission = "0600"
+}
+
+resource "local_file" "ansible_inventory" {
+  content = yamlencode({
+    traccar-servers = {
+      hosts = {
+        for server in aws_lightsail_instance.traccar_server: server.name => {
+          ansible_host = server.public_ip_address
+        }
+      }
+    }
+  })
+  filename = "${path.module}/inventory.yaml"
+}
+
+data "aws_secretsmanager_secret" "dockerhub_credentials" {
+  name = "DockerhubCredentials"
+}
+
+data "aws_secretsmanager_secret_version" "dockerhub_credentials" {
+  secret_id = data.aws_secretsmanager_secret.dockerhub_credentials.id
+}
+
+resource "local_sensitive_file" "traccar_servers_docker_credentials" {
+  content = yamlencode({
+    docker_username = jsondecode(data.aws_secretsmanager_secret_version.dockerhub_credentials.secret_string)["username"]
+    docker_password = jsondecode(data.aws_secretsmanager_secret_version.dockerhub_credentials.secret_string)["password"]
+  })
+  filename = "${path.module}/group_vars/traccar-servers/secret_docker_credentials.yaml"
 }
